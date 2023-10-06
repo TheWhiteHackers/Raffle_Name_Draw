6fa65a89b17a447a42e6a71295cb410576248dbb
TheWhiteHackers <nurefsanbera@gmail.com>
2023-10-05 20:44:34 -0500

    still trying...

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 let selectedprize = "";
+let removeamount = 10;
+let removespeed = 500;
 
 let listofnames = ["Alma Watts", "Ishaan Ochoa", "Zoie Williamson", "Kelsie Melendez", "Cierra Walsh", "Rayna Colon", "Keon Bryant", "Gordon Hogan", "Rachael Munoz", "Aliana Compton", "Chasity Roberson", "Gregory Castillo", "Aldo Blankenship", "Naomi Phillips", "Delaney Heath", "Yadiel Kirk", "Shyanne Cook", "Katie Barker", "Erik Rasmussen", "Audrey Oconnell", "Keon Hogan", "Alvin Herrera", "Brooklynn Larson", "Jennifer Drake", "Izabella Salas", "Dayton Clarke", "Matteo Torres", "Kylan Steele", "Erin Hill", "Keyon Golden", "Thaddeus Haley", "Cedric Hood", "Kenley Ross", "Estrella Colon", "Genesis Salinas", "Christian Robbins", "Makenzie Summers", "Ashtyn Wilkins", "Mckenna Arellano", "Kelsey Arias", "Kenzie Fry", "Ali Little", "Lacey Adams", "Dexter Greene", "Halle Buckley", "Charlotte Woods", "Kayleigh Larsen", "Tianna Farmer", "Riya Chapman", "Bryce Fox", "Talan Gray", "Isabela Wade", "Maren Lynn", "Cherish Bradley", "Lewis Burton", "David Cherry", "Alia Hughes", "Kamari Pearson", "Madelyn Mcpherson", "Esmeralda Riggs", "Ireland Ewing", "Cassius Case", "Ingrid Cherry", "Harper Livingston", "Brody Leach", "Silas Ellison", "Anne Gillespie", "Teagan Garner", "Malaki Juarez", "Silas Turner", "Roland Thornton", "Kadyn Parker", "Leyla Herman", "Kaleb Pierce", "Larry Mcdowell", "Sophia Mendoza", "Alisa Simmons", "Kristin Griffin", "Claire Shepherd", "Julio Ford", "Destiney Haas", "Clark Macias", "Miriam Mcgee", "Kierra Leach", "Justus Pham", "Emely Cameron", "Laura Petersen", "Arthur Benton", "Rodney Carter", "Skyla Strickland", "Jazmin Rowe", "Kasey Moreno", "Junior Boyer", "Journey Mendez", "Dulce Thomas", "Chad Mcgee", "Halle Long", "Isis Robles", "Micheal Perez", "Immanuel Stout", "Kadyn Ibarra", "Aiden Stewart", "Ali Aguilar", "Alyssa Fletcher", "Aliana Robles", "Branden Orozco", "Lane Martin", "Dominic Hodge", "Kaylyn Leach", "Lizbeth Morales", "Addyson Cruz", "Greta Park", "Ruby Haas", "Duncan Rocha", "Derek Garcia", "Kailey Hart", "Nathaniel Jarvis", "Malik Mason", "Faith Shea", "Adelyn Case", "Breanna Castaneda", "Prince Price", "Saniya Ferrell", "Maia Weiss", "Elian Mcgrath", "Dania Rice", "Timothy Wilcox", "Rosemary Davenport", "Lily Leonard", "Marvin Pineda", "Jonathon Pierce", "Kali Blair", "Nataly Waller", "Cordell Moses", "Raven Compton", "Walter Bonilla", "Cara Clarke", "Issac Blackwell", "Solomon Cantu", "Alena Barron", "Jocelynn Reynolds", "Annalise Benitez", "Cailyn Webb", "Kathy Robinson", "Margaret Mcgee", "Frank Bond", "Piper Atkinson", "Delilah Adams", "Gisselle Contreras", "Amiah Contreras", "Annabella Smith", "Esmeralda Mckay", "Samuel Watts", "Alina Sosa", "Simeon Lynch", "Eduardo Gallegos", "Julia Rangel", "Sidney Fuller", "Mckenzie Wilcox", "Kendrick Hooper", "Taniyah Ochoa", "Vance Campbell", "Dayami Kramer", "Elsa Harrell", "Karlee Mccall", "Lana Mathews", "Saige Andersen", "Melany Tapia", "Kaitlynn Rosales", "Perla Benton", "Savannah Sims", "Alessandra Jefferson", "Zachariah Porter", "Adalyn Shaffer", "Harmony Fisher", "Jaqueline Zhang", "Daniela Stone", "Daniela Wiggins", "Alijah Clarke", "Caleb Love", "Keon Crawford", "Aron Nolan", "Brian Barron", "Makenna Black", "Brielle Whitaker", "Celia Lutz", "Terry Mora", "Darius Armstrong", "Devin Kane", "Madden Dennis", "Heidy Fletcher", "Amani Thornton", "Brice Roy", "Cara Burton", "Yaritza Kramer", "Junior Gay", "Caden Osborn", "Shania Barrett", "Edwin Webster", "Maximo Richardson"];
 
@@ -92,7 +94,7 @@ function rafflebegin(id){
 
     let namepush = "";
     for (let i = 0; i < listofnames.length; i++) {
-        namepush+="<div class=mininame id='div_" + i + "'>"+listofnames[i].replace(' ', '&nbsp;')+"</div>"
+        namepush+="<div class=mininame>"+listofnames[i].replace(' ', '&nbsp;')+"</div>"
     }
     document.getElementById("namediv").innerHTML=namepush;
 
@@ -101,29 +103,69 @@ function rafflebegin(id){
 }
 
 
-// Function to select a random div, apply the special class, and remove it from the screen and array after a timeout
 function selectRandomDiv() {
-    // Check if there are unselected divs
-    if (listofnames.length > 0) {
+    // let removelist = [];
+    // if (listofnames.length > 1) { 
+    //     for (let i = 0; i < removeamount; i++) {
+    //         let selectedDivIndex = Math.floor(Math.random() * listofnames.length);
+    //         let selectedDiv = document.querySelector('.mininame:nth-child(' + (selectedDivIndex + 1) + ')');
+    //         selectedDiv.classList.add('flasheffect');
+    //         removelist.push([selectedDiv, selectedDivIndex]);
+    //     }
+    //     console.log(removelist);
+    //     setTimeout(function() {
+    //         for (let i = 0; i < removeamount; i++) {
+    //             removelist[i][0].style.display="none";
+    //             listofnames.splice(removelist[i][1], 1);
+    //         }
+    //         removelist=[];
+    //         selectRandomDiv();
+    //     }, removespeed);
+
+    // } else {
+    //     console.log('All divs have been removed.');
+    // }
+
+    var selectedDivIndices = [];
+    
+    // Select 10 random divs
+    while (selectedDivIndices.length < 10 && listofnames.length > 1) {
         var randonum = Math.floor(Math.random() * listofnames.length);
-        var selectedDivIndex = randonum;
-
-        // Get the corresponding div element by ID
-        var selectedDiv = document.querySelector('.mininame:nth-child(' + (selectedDivIndex + 1) + ')');
-
-        // Add the special class 'flasheffect' to the selected div
-        selectedDiv.classList.add('flasheffect');
-
-        // Remove the selected div from the screen after a timeout
-        setTimeout(function() {
-            selectedDiv.remove(); // Remove from the DOM
-            listofnames.splice(selectedDivIndex, 1);
-            console.log(listofnames.length);
-            selectRandomDiv()
-            // Remove from the list
-        }, 2000); // Replace '2000' with the desired delay time in milliseconds (e.g., 2000ms = 2 seconds)
-    } else {
-        // All divs have been removed
-        console.log('All divs have been removed.');
+        selectedDivIndices.push(randonum);
     }
+
+    // Apply the special class 'flasheffect' to the selected divs
+    for (var i = 0; i < selectedDivIndices.length; i++) {
+        var selectedDivIndex = selectedDivIndices[i];
+        var selectedDiv = document.querySelector('.mininame:nth-child(' + (selectedDivIndex + 1) + ')');
+        
+        // Check if the selectedDiv exists before applying the class
+        if (selectedDiv) {
+            selectedDiv.classList.add('flasheffect');
+        }
+    }
+
+    // Remove the selected divs from the screen and array after a timeout
+    setTimeout(function() {
+        for (var i = selectedDivIndices.length - 1; i >= 0; i--) {
+            var selectedDivIndex = selectedDivIndices[i];
+            var selectedDiv = document.querySelector('.mininame:nth-child(' + (selectedDivIndex + 1) + ')');
+            
+            // Check if the selectedDiv exists before removing it
+            if (selectedDiv) {
+                selectedDiv.remove(); // Remove from the DOM
+                listofnames.splice(selectedDivIndex, 1); // Remove from the list
+            }
+        }
+
+        // Check if there are more divs to process
+        if (listofnames.length > 0) {
+            // Call the function again to process the next batch of divs
+            selectRandomDiv();
+        } else {
+            // All divs have been removed
+            console.log('All divs have been removed.');
+        }
+    }, 2000); // Replace
+    
 }
